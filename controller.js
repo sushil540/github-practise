c298127059446dcebbdcfba2e9108016d3bfbfd9
sushil <gpsushil@gmail.com>
2023-06-23 18:58:24 +0530

    added User model

--- controller.js
+++ controller.js
@@ -1,9 +1,13 @@
+const User = require("./User")
+
 const userController = {}
+
 userController.get = async (req, res) => {
     try {
-        const user = findByid
+        const user = await User.find()
+        res.json(user)
     } catch (error) {
-
-    }
+        res.json(error)
+    }   
 }
 module.exports = userController
